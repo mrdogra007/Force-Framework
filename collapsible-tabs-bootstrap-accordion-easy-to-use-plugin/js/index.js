27ae320af280b50afd139fcbd10d89f005781310
Nishant Dogra <13938655+mrdogra007@users.noreply.github.com>
2019-02-07 17:22:20 +0530

    Force Framework updates GIT
    Force Framework updates GIT

--- collapsible-tabs-bootstrap-accordion-easy-to-use-plugin/js/index.js
+++
@@ -1,5 +0,0 @@
-// Collapsible tabs (Bootstrap accordion) 
-// Deigned by Nishant Dogra
-// Date: 07/12/2017
-// Twitter: @mrdogra007
-// Blog: https://t.co/F6RF2rCHu3
---
+++ plugins/simple-html-css-management-tree-tree-architecture-plugin/js/index.js
@@ -0,0 +1,5 @@
+// Simple HTML/CSS Management Tree, Tree Architecture plugin - Force Framework
+// Deigned by Nishant Dogra
+// Date: 09/05/2018
+// Twitter: @mrdogra007
+// Website: http://dograsweblog.com/drive/force-framework/
