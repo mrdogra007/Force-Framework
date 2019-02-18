297febfad3668fac88a70331764df87f3ad9e1d5
Nishant Dogra <13938655+mrdogra007@users.noreply.github.com>
2019-02-18 17:31:56 +0530

    Pure CSS cards widget - Force Framework
    Pure CSS cards / Deal and Offer cards widget - Force Framework

---
+++ plugins/pure-css-cards-deal-and-offer-cards-widget-force-framework/js/index.js
@@ -0,0 +1,8 @@
+// Pure CSS cards - Force Framework
+// Deal and Offer cards for ecommerce websites and stores, side widgets for offer cards
+// Crafted on: Force Framework
+// Created: 18/01/2018
+// Updated: 18/02/2019
+// Author: Nishant Dogra
+// Twitter: @mrdogra007
+// http://dograsweblog.com/drive/force-framework/
