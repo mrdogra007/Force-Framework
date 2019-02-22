46230430190e11f22b60ec93df1a197a05187bee
Nishant Dogra <13938655+mrdogra007@users.noreply.github.com>
2019-02-22 15:54:47 +0530

    Pure CSS based 4 tabs plugin
    Pure CSS based 4 tabs plugin - Force Framework

---
+++ plugins/pure-css-based-4-tabs-plugin-force-framework/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/mrdogra007/pen/ZwdJbB.
+
+ Pure CSS based 4 tabs plugin designed for Force Framework, easy to plug and play. Simple and clean, no javascript. Crafted on Force Framework. 
---
+++ plugins/pure-css-based-4-tabs-plugin-force-framework/js/index.js
@@ -0,0 +1,6 @@
+// Pure CSS based 4 tabs plugin
+// Pure CSS based 4 tabs plugin designed for Force Framework, easy to plug and play
+// Deigned by Nishant Dogra
+// Created: 22/02/2019
+// Twitter: @mrdogra007
+// http://dograsweblog.com/drive/force-framework/
