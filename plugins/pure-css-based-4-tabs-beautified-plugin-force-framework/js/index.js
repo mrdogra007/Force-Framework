136013e994846cfe80138271a5b4792883040501
Nishant Dogra <13938655+mrdogra007@users.noreply.github.com>
2019-02-22 16:43:27 +0530

    Pure CSS based 4 tabs plugin
    Pure CSS based 4 tabs beautified plugin - Force Framework

---
+++ plugins/pure-css-based-4-tabs-beautified-plugin-force-framework/js/index.js
@@ -0,0 +1,6 @@
+// Pure CSS based 4 tabs plugin beutified
+// Pure CSS based 4 tabs plugin beutified designed for Force Framework, easy to plug and play
+// Deigned by Nishant Dogra
+// Created: 22/02/2019
+// Twitter: @mrdogra007
+// http://dograsweblog.com/drive/force-framework/
