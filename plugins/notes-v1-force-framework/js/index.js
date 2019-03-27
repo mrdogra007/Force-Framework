1b6ba5e8bf61e61a30239e7fd53f9bc713a14805
Nishant Dogra <13938655+mrdogra007@users.noreply.github.com>
2019-03-27 23:58:46 +0530

    notes-v1-force-framework
    notes-v1-force-framework

---
+++ plugins/notes-v1-force-framework/js/index.js
@@ -0,0 +1,6 @@
+// Notes plugin - Force Framework
+// Deigned by Nishant Dogra
+// Created: 27/03/2018
+// Updated: 27/03/2018
+// Twitter: @mrdogra007
+// http://dograsweblog.com/drive/force-framework/
