394e59fa1b5cc3b61cbe5476c90f01b6223694bb
Nishant Dogra <13938655+mrdogra007@users.noreply.github.com>
2019-04-26 17:06:49 +0530

    Force Framework plugin
    Force Framework plugin

---
+++ plugins/pinned-notes-tilted-plugin-force-framework/js/index.js
@@ -0,0 +1,6 @@
+// Notes scrolable plugin  - Force Framework
+// Deigned by Nishant Dogra
+// Created: 26/04/2018
+// Updated: 26/04/2018
+// Twitter: @mrdogra007
+// http://dograsweblog.com/drive/force-framework/
