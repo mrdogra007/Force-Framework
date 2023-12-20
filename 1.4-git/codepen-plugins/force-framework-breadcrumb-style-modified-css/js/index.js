f67b72893939b04b838bfcb9cc1447644ff4f993
Nishant Dogra <mr.dogra.007@gmail.com>
2023-12-20 13:11:54 +0530

    Delete 1.4-git/codepen-plugins directory

--- 1.4-git/codepen-plugins/force-framework-breadcrumb-style-modified-css/js/index.js
+++
@@ -1,5 +0,0 @@
-// Force Framework breadcrumb style modified CSS #ForceFramework
-// Deigned by Nishant Dogra
-// Date: 22/03/2018
-// Twitter: @mrdogra007
-// Website: http://dograsweblog.com/drive/force-framework/
--- 1.4-git/codepen-plugins/force-framework-pagination-style-modified-css/js/index.js
+++
@@ -1,5 +0,0 @@
-// Pagination plugin CSS & HTML only - Force Framework
-// Deigned by Nishant Dogra
-// Date: 20/08/2018
-// Twitter: @mrdogra007
-// Website: http://dograsweblog.com/drive/force-framework/
